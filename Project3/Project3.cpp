5aac96bf653db8257504d5df6181624f325fdeb3
Summer Zhang <xm1994@gmail.com>
2014-12-16 18:10:20 +0800

    Project3 already finished. change test.txt not important

--- Project3/Project3.cpp
+++ Project3/Project3.cpp
@@ -3,13 +3,15 @@
 #include <queue>
 #include <algorithm>
 #include <cstdio>
+#include <bitset>
 using namespace std;
 
 struct TreeNode
 {
 	int Freq;
-	unsigned char Chr;
+	char Chr;
 	bool hasChr;
+	short Word;
 	struct TreeNode *LChild;
 	struct TreeNode *RChild;
 	bool operator < (struct TreeNode & rhs)
@@ -21,7 +23,7 @@ struct TreeNode
 		return(this->Freq > rhs.Freq);
 	}
 };
-struct TreeNode Chars[256];
+struct TreeNode Chars[128];
 
 struct NodeComp
 {
@@ -35,10 +37,11 @@ priority_queue<struct TreeNode*, vector<struct TreeNode*>, NodeComp > q;
 
 void InitChars()
 {
-	for (int i = 0; i < 256; ++i)
+	for (int i = 0; i < 128; ++i)
 	{
 		Chars[i].Chr = i;
 		Chars[i].Freq = 0;
+		Chars[i].Word = 32767;
 		Chars[i].hasChr = true;
 		Chars[i].LChild = nullptr;
 		Chars[i].RChild = nullptr;
@@ -57,7 +60,7 @@ void CountChars(FILE *fp)
 }
 void AddToQueue(priority_queue<struct TreeNode*, vector<struct TreeNode*>,NodeComp > &q)
 {
-	for (int i = 0; i < 256; i++)
+	for (int i = 0; i < 128; i++)
 		q.push(Chars+i);
 }
 
@@ -68,6 +71,11 @@ struct TreeNode *MakeHuff(priority_queue<struct TreeNode*, vector<struct TreeNod
 	{
 		t1 = q.top();
 		q.pop();
+		while (!(t1->Freq > 0))
+		{
+			t1 = q.top();
+			q.pop();
+		};
 		t2 = q.top();
 		q.pop();
 		t = new TreeNode;
@@ -82,17 +90,72 @@ struct TreeNode *MakeHuff(priority_queue<struct TreeNode*, vector<struct TreeNod
 	}
 	return t;
 }
+int tt = 0;
+void MakeDict(TreeNode *troot,unsigned int word,int deep)
+{
+	if (deep > tt)
+		tt = deep;
+	troot->Word = word;
+	if(troot->LChild)
+		MakeDict(troot->LChild,  word << 1,deep+1);
+	if (troot->RChild)
+		MakeDict(troot->RChild, (word << 1) + 1,deep+1);
+}
+short dict[128];
 
-int main(void)
+void Compress()
 {
 	InitChars();
 	char buf[128];
 	printf("请输入要编码的文件名：");
-	scanf("%s",buf);
+	scanf("%s", buf);
 	FILE *fp = fopen(buf, "r");
 	CountChars(fp);
+	fclose(fp);
 	AddToQueue(q);
 	TreeNode *root = MakeHuff(q);
+	MakeDict(root, 0, 1);
+	for (int i = 0; i < 128; i++)
+		dict[i] = Chars[i].Word;
+	printf("%d %d\n", tt, sizeof(short));
+	fp = fopen(buf, "r");
+	FILE *fw = fopen("compress.dat", "wb");
+	char ch = fgetc(fp);
+	while (!feof(fp))
+	{
+		fwrite(&dict[ch], sizeof(short), 1, fw);
+		ch = fgetc(fp);
+	}
+	fclose(fp);
+	fclose(fw);
+	fp = fopen(strcat(buf, ".dicdb"), "wb+");
+	fwrite(dict, sizeof(dict), 1, fp);
+	fclose(fp);
+
+}
+void Decompress()
+{
+	FILE *dic = fopen("test.txt.dicdb", "rb");
+	fread(dict, sizeof(dict), 1, dic);
+	fclose(dic);
+	FILE *fr = fopen("compress.dat", "rb");
+	FILE *fw = fopen("decompress.txt", "w+");
+	short cs;
+	fread(&cs, sizeof(short), 1, fr);
+	while (!feof(fr))
+	{
+		for (int i = 0; i < 128; i++)
+			if (dict[i] == cs)
+				fputc(i, fw);
+		fread(&cs, sizeof(short), 1, fr);
+	}
+	fclose(fr);
+	fclose(fw);
+}
+int main(void)
+{
+	//Compress();
+	Decompress();
 	system("pause");
 	return 0;
 }
